86cbdf8049b31b505917bc2ba1c7f6783692ab84
Aluya0507 <56738022+Aluya0507@users.noreply.github.com>
2020-10-17 03:53:33 -0500

    Add files via upload

--- Library.java
+++ Library.java
@@ -1,179 +1,278 @@
 // --== CS400 File Header Information ==--
-// Name: Yuliang Peng
-// Email: peng68@wisc.edu
-// Team: EB
-// Role: Test Engineer
-// TA: Keren Chen
-// Lecturer: Florian Heimerl
+// Name: <Haoxuan Lu>
+// Email: <hlu224@wisc.edu email address>
+// Team: <EB>
+// Role: <Back End Developer>
+// TA: <Keren Chen>
+// Lecturer: <Florian Heimerl>
 // Notes to Grader: <optional extra notes>
 
 import java.util.LinkedList;
 
 /**
- * This class represents a Library to store Books using a RBTree.
- * @author Yuliang Peng
+ * This is a library class used to store or get out books
+ * 
+ * @author Haoxuan Lu
+ *
  */
 public class Library {
-    
+
   /**
-   * This nested class represents an individual Book element that will be stored in Library.
-   * @author Yuliang Peng
+   * This is a book class used to express various characters of book object
+   * 
+   * @author Haoxuan
+   *
    */
   public static class Book implements Comparable<Book> {
+
     private int ISBN;
     private String title;
     private String author;
     private boolean checked; // represents the book's availability status
-    
-    public Book(int ISBN, String title, String author) throws NullPointerException{
-      if (ISBN==0 || title==null || author==null) throw new NullPointerException();
-      this.ISBN=ISBN;
-      this.title=title;
-      this.author=author;
-      this.checked=false; // initially available (not checked out)
-    }
-    
-    public int getISBN() {
-      return ISBN;
-    }
-    
-    public String getTitle() {
-      return title;
-    }
-    
-    public String getAuthor() {
-      return author;
-    }
-    
+
     /**
-     * Two Book objects are compared according to their ISBN number.
-     * Comparing to a larger ISBN will result in a positive number, comparing to a smaller ISBN will result in a negative number,
+     * The constructor of book variable used to create a book object
+     * 
+     * @param ISBN   an integer represent the ISBN number of the book
+     * @param title  a string represent the title of the book
+     * @param author a string represent the author name of the book
+     */
+    public Book(int ISBN, String title, String author) {
+      this.ISBN = ISBN;
+      this.title = title;
+      this.author = author;
+      this.checked = false; // initially available (not checked out)
+    }
+
+    /**
+     * The default constructor used to create a null book object
+     */
+    public Book() {
+      this(0, null, null);
+    }
+
+    /**
+     * The getter used to get the ISBN number for the book
+     * 
+     * @return an integer represent the ISBN number of the book
+     */
+    public int getISBN() {
+      return this.ISBN;
+    }
+
+    /**
+     * The getter used to get the title of the book
+     * 
+     * @return a string represent the title of the book
+     */
+    public String getTitle() {
+      return this.title;
+    }
+
+    /**
+     * The getter used to get the author name of the book
+     * 
+     * @return a string represent the author name of the book
+     */
+    public String getAuthor() {
+      return this.author;
+    }
+
+    /**
+     * The setter used to set or change the ISBN of the book
+     * 
+     * @param ISBN an int variable represent the ISBN number of the book
+     */
+    public void setISBN(int ISBN) {
+      this.ISBN = ISBN;
+    }
+
+    /**
+     * The setter used to set or change the title of the book
+     * 
+     * @param title an string variable represent the title of the book
+     */
+    public void setTitle(String title) {
+      this.title = title;
+    }
+
+    /**
+     * The setter used to set or change the title of the book
+     * 
+     * @param author an string variable represent the author of the book
+     */
+    public void setAuthor(String author) {
+      this.author = author;
+    }
+
+    /**
+     * Two Book objects are compared according to their ISBN number. Comparing to a larger ISBN will
+     * result in a positive number, comparing to a smaller ISBN will result in a negative number,
      * comparing to the same ISBN will result in 0.
      */
     @Override
     public int compareTo(Book book) {
-      return this.ISBN-book.ISBN;
+      return this.ISBN - book.ISBN;
     }
-    
   }
-  
-  private RedBlackTree<Book> bookShelf=new RedBlackTree<>(); // the RBTree Books will be stored in.
-  
-  /**
-   * Adding an individual Book object into the Library.
-   * @param ISBN - ISBN of the book;
-   * @param title - title of the book;
-   * @param author - author of the book.
-   * @return true if added successfully, false otherwise.
-   */
-  public boolean add(int ISBN, String title, String author) {
-    try {
-      Book newBook=new Book(ISBN,title,author); // the Book to be added
-      bookShelf.insert(newBook);
-    } catch (Exception e) { // if the book isn't added successfully (either because of a duplication or a null input)
-      System.out.println(e.getMessage());
-      return false;
-    }
-    return true;
-  }
-  
-  /**
-   * Return corresponding Book of the specified ISBN.
-   * @param ISBN of the Book user is looking for.
-   * @return the corresponding Book of the ISBN.
-   */
-  public Book getBook(int ISBN) {
-    RedBlackTree.Node<Book> checkedBook=bookShelf.root;
-    while (checkedBook.data.ISBN!=ISBN) { // find the book with the specified ISBN from root node
-      if (checkedBook.data.ISBN<ISBN ) { 
-        if (checkedBook.rightChild!=null)
-          checkedBook=checkedBook.rightChild;
-        else { // if the ISBN is not valid
-          checkedBook=null;
-          break;
-        }
-      }
-      else if (checkedBook.data.ISBN>ISBN) {
-        if (checkedBook.leftChild!=null)
-          checkedBook=checkedBook.leftChild;
-        else { // if the ISBN is not valid
-          checkedBook=null;
-          break;
-        }
-      }
-    }
-    if (checkedBook!=null) return checkedBook.data;
-    return null; // no Book with the ISBN exists
-  }
-  
-  /**
-   * Simulate checking-out process of a book with specified ISBN.
-   * @param the ISBN of Book user wants to check out.
-   * @return true if checked out successfully, false otherwise.
-   */
-  public boolean checkOut(int ISBN) {
-    if (getBook(ISBN)==null || getBook(ISBN).checked==true) return false; // Book doesn't exist or is checked out already
-    getBook(ISBN).checked=true; // Book checked out, set status to checked-out
-    return true;
-  }
-  
-  /**
-   * Simulate checking-in process of a book with specified ISBN.
-   * @param the ISBN of Book user wants to check in.
-   * @return true if checked in successfully, false otherwise.
-   */
-  public boolean checkIn(int ISBN) {
-    if (getBook(ISBN)==null) return false; // Book doesn't belong to the Library
-    getBook(ISBN).checked=false; // Book checked in, set status to not checked-out
-    return true;
-  }
-  
-  /**
-   * Check the availability (status) of the Book with certain ISBN.
-   * @param the ISBN of Book that is checked
-   * @return true if the book is in the Library, false otherwise
-   */
-  public boolean checkAvailability(int ISBN) {
-    if (getBook(ISBN)==null || getBook(ISBN).checked==true) return false; // Book not in the Library (either doesn't belong to it
-                                                                          // or has already been checked out
-    return true;
-  }
-  
-  /**
-   * Print the Books in Library in a level order traversal way, each Book's information will be printed as follows:
-   * "ISBN: <Book's ISBN>
-   *  Title: <Book's title>
-   *  Author: <Book's author>"
-   * @return a String of all the Book information
-   */
-  public String toString() {
-    String output = "";
-    LinkedList<RedBlackTree.Node<Book>> q = new LinkedList<>();
-    q.add(bookShelf.root);
-    while(!q.isEmpty()) {
-        RedBlackTree.Node<Book> next = q.removeFirst();
-        if(next.leftChild != null) q.add(next.leftChild);
-        if(next.rightChild != null) q.add(next.rightChild);
-        output =output+"ISBN: "+next.data.ISBN+"\nTitle: "+next.data.title+"\nAuthor: "+next.data.author;
-        if(!q.isEmpty()) output += "\n\n";
-    }
-    return output;
-  }
-  
-  /**
-   * Simple visualization of the Library class.
-   * @param args input arguments if any
-   */
-  public static void main(String[] args) {
-    Library bookShelf=new Library();
-    bookShelf.add(286, "trwg", "aldb");
-    bookShelf.add(765, "wifg", "slng");
-    bookShelf.add(45421, "sufg", null);
 
-    bookShelf.add(134,"ahefoh","sui");
+    private RedBlackTree<Book> bookShelf;
+
+    public Library() {
+      this.bookShelf = bookShelf = new RedBlackTree<>();
+    }
+      
     
-    System.out.println(bookShelf.checkAvailability(134));
-    System.out.println(bookShelf.toString());
-  }
+    /**
+     * Adding an individual Book object into the Library.
+     * 
+     * @param ISBN   - ISBN of the book;
+     * @param title  - title of the book;
+     * @param author - author of the book.
+     * @return true if added successfully, false otherwise.
+     */
+    public boolean addBook(int ISBN, String title, String author) {
+      try {
+        Book newBook = new Book(ISBN, title, author); // the Book to be added
+        bookShelf.insert(newBook);
+        return true;
+      } catch (Exception e) {
+        e.printStackTrace();
+        return false;
+      }
+    }
+
+    /**
+     * This method used to check whether the book with certain ISBN is in the library
+     * 
+     * @param ISBN of the Book user is looking for.
+     * @return true if the book with this ISBN in the library, false otherwise
+     */
+    public boolean containsBook(int ISBN) {
+      return getBook(ISBN) != null;
+    }
+
+    /**
+     * Return corresponding Book of the specified ISBN.
+     * 
+     * @param ISBN of the Book user is looking for.
+     * @return the corresponding Book of the ISBN.
+     */
+    public Book getBook(int ISBN) {
+      RedBlackTree.Node<Book> checkedBook = bookShelf.root;
+      while (checkedBook.data.ISBN != ISBN) { // find the book with the specified ISBN from root
+                                              // node
+        if (checkedBook.data.ISBN < ISBN) {
+          if (checkedBook.rightChild != null)
+            checkedBook = checkedBook.rightChild;
+          else { // if the ISBN is not valid
+            checkedBook = null;
+            break;
+          }
+        } else if (checkedBook.data.ISBN > ISBN) {
+          if (checkedBook.leftChild != null)
+            checkedBook = checkedBook.leftChild;
+          else { // if the ISBN is not valid
+            checkedBook = null;
+            break;
+          }
+        }
+      }
+      if (checkedBook != null)
+        return checkedBook.data;
+      return null; // no Book with the ISBN exists
+    }
+
+    /**
+     * Get the title of specific book with its ISBN number
+     * 
+     * @param ISBN of the Book user is looking for.
+     * @return a string represents the title of the specific book
+     */
+    public String getBookTitle(int ISBN) {
+      if (containsBook(ISBN))
+        return getBook(ISBN).getTitle();
+      System.out.println("Sorry, this book is not in the library");
+      return null;
+    }
+
+    /**
+     * Get the author of specific book with its ISBN number
+     * 
+     * @param ISBN of the Book user is looking for.
+     * @return a string represents the author of the specific book
+     */
+    public String getBookAuthor(int ISBN) {
+      if (containsBook(ISBN))
+        return getBook(ISBN).getAuthor();
+      System.out.println("Sorry, this book is not in the library");
+      return null;
+    }
+
+    /**
+     * Simulate checking-out process of a book with specified ISBN.
+     * 
+     * @param the ISBN of Book user wants to check out.
+     * @return true if checked out successfully, false otherwise.
+     */
+    public boolean checkOut(int ISBN) {
+      if (!containsBook(ISBN) || getBook(ISBN).checked) {
+        System.out.println("Sorry this book is not in the library or have been checked out.");
+        return false; // Book doesn't exist or is checked out already
+      }
+
+      getBook(ISBN).checked = true; // Book checked out, set status to checked-out
+      return true;
+    }
+
+    /**
+     * Simulate checking-in process of a book with specified ISBN.
+     * 
+     * @param the ISBN of Book user wants to check in.
+     * @return true if checked in successfully, false otherwise.
+     */
+    public boolean checkIn(int ISBN) {
+      if (!containsBook(ISBN)) {
+        System.out.println("Sorry, this book is not belong to our library");
+        return false;
+      }
+
+      getBook(ISBN).checked = false; // Book checked in, set status to not checked-out
+      return true;
+    }
+
+    /**
+     * Check the availability (status) of the Book with certain ISBN.
+     * 
+     * @param the ISBN of Book that is checked
+     * @return true if the book is in the Library, false otherwise
+     */
+    public boolean isAvailabile(int ISBN) {
+      return containsBook(ISBN) && !(getBook(ISBN).checked);
+    }
+
+    /**
+     * Print the Books in Library in a level order traversal way, each Book's information will be
+     * printed as follows: "ISBN: <Book's ISBN> Title: <Book's title> Author: <Book's author>"
+     * 
+     * @return a String of all the Book information
+     */
+    public String toString() {
+      String output = "";
+      LinkedList<RedBlackTree.Node<Book>> q = new LinkedList<>();
+      q.add(bookShelf.root);
+      while (!q.isEmpty()) {
+        RedBlackTree.Node<Book> next = q.removeFirst();
+        if (next.leftChild != null)
+          q.add(next.leftChild);
+        if (next.rightChild != null)
+          q.add(next.rightChild);
+        output = output + "ISBN: " + next.data.ISBN + "\nTitle: " + next.data.title + "\nAuthor: "
+            + next.data.author;
+        if (!q.isEmpty())
+          output += "\n\n";
+      }
+      return output;
+    }
 
 }
